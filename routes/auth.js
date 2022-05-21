54d5cf6fe4fb8fed433ecae3db18e3ff31df4285
Ayush Rathore <rathore.ayush9@gmail.com>
2022-05-21 12:58:33 +0530

    fix: Add password hashing, jwt login

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'buldozer';
 
 router.post('/create', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
@@ -19,16 +23,55 @@ router.post('/create', [
         if (user)
             return res.status(400).json({ err: "User already exists." })
 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
         let newUser = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPassword,
         })
 
-        return res.status(200).json(newUser)
+        const data = {
+            user: {
+                id: newUser.id
+            }
+        }
+        var authToken = jwt.sign(data, JWT_SECRET);
+
+        return res.status(200).json({ authToken })
     } catch (err) {
         console.log(err.message);
         return res.status(500).json({ err: "Something went wrong while creating a user" })
+    }
+})
+
+router.get('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be empty').exists(),
+], async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email })
+        if (!user)
+            return res.status(400).json({ err: "user does not exist" })
+
+        const correctPassword = await bcrypt.compare(password, user.password)
+        if (!correctPassword)
+            return res.status(400).json({ err: "invalid credentials" })
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        var authToken = jwt.sign(data, JWT_SECRET);
+
+        return res.status(200).json({ authToken })
+    } catch (err) {
+        console.log(err.message);
+        return res.status(500).json({ err: "Something went wrong while sign in" })
     }
 
 })
